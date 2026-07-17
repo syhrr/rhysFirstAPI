55769ec923d6dbd05fd0b41cf86f9f7168c2af5c
efyrg2 <efyrg2@nottigham.ac.uk>
2026-07-17 22:00:45 +0100

    initialised and first steps

---
+++ server.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+
+const Database = require('better-sqlite3')
+app.use(express.json())
+
+// create a new DB
+const db = new Database('todo.db')
+
+// create the schema for the DB
+db.exec(`
+  CREATE TABLE IF NOT EXISTS users (
+    id   INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL
+  )
+`)
+
+// when someone sends a GET request to the ROOT of the server
+// this happens
+app.get('/',(req,res) =>{
+    res.send('Hello from server')
+})
+
+
+// when someone sends a GET request to /register
+// this happens
+app.get('/register',(req,res) =>{
+    res.send('Register page')
+})
+
+// when you send a POST request to the /register route 
+// return a 201 response json
+app.post('/register', async (req, res) => {
+    const { name, email, password} = req.body
+
+    if (!name || !password || !email ) return res.status(400).json({ error: 'name, password and email are required' })
+
+    const hash = await bcrypt.hash(password, 12)
+        
+ try {
+    const result = db.prepare(
+      'INSERT INTO users (name, email, password_hash) VALUES (?, ?, ?)'
+    ).run(name, email, hash)
+    res.status(201).json({ id: result.lastInsertRowid, name })
+  } catch (err) {
+    if (err.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+      return res.status(409).json({ error: 'email already taken ffs' })
+    }
+    throw err
+  }
+})
+
+// get all users if you go to /users route
+app.get('/users', (req,res) => {
+    const users = db.prepare('SELECT * FROM users').all()
+    res.json(users)
+
+})
+
+
+// GET  for login page
+app.get('/login',(req,res) => {
+  res.send('Pls send login details')
+})
+
+
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    
+    // 400 bad request
+    return res.status(400).json({ error: 'email and password are required' })
+  }
+
+  const user = db.prepare('SELECT * FROM users WHERE email = ?').get(email)
+  // use 401 instead of 404, 401 not authorised
+  if (!user) return res.status(401).json({ error: 'user or password is incorrect' })
+
+  // bcrypt compares the password with the hashed password stored in the DB
+  const match = await bcrypt.compare(password, user.password_hash)
+
+  if (!match) return res.status(401).json({ error: 'user or password is incorrect' })
+  res.json({ id: user.id, name: user.name })
+  
+
+
+})
+
+
+
+// listens on port 3000
+app.listen(3000,() => {
+    console.log("server is running")
+})
+
